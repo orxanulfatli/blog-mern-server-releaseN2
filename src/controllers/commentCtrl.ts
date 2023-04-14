b5e571ec81559b47881a4edeb11054d9c091072e
Orxan Ulfatli <orxanulfatli@gmail.com>
2023-04-15 00:21:35 +0400

    update and delete comment

--- src/controllers/commentCtrl.ts
+++ src/controllers/commentCtrl.ts
@@ -13,216 +13,218 @@ const Pagination = (req: IReqAuth) => {
     return { page, limit, skip };
 }
 class CommentController {
-    createComment = async (req:IReqAuth,res:Response,next:NextFunction) => {
-     try {
-         if (!req.user) throw ApiError.UnauthorizedError();
-         const {
-             content,
-             blog_id,
-             blog_user_id
-         } = req.body
-
-         const newComment = new Comments({
-             user: req.user._id,
-             content,
-             blog_id,
-             blog_user_id
-         });
-         await newComment.save()
-
-         return res.json(newComment)
-     } catch (error) {
-         next(error)
-     }
-    }
-
-    getComments= async (req: Request, res: Response,next:NextFunction) => {
-    const { limit, skip } = Pagination(req)
-
-    try {
-        const data = await Comments.aggregate([
-            {
-                $facet: {
-                    totalData: [
-                        {
-                            $match: {
-                                blog_id: new mongoose.Types.ObjectId(req.params.id),
-                                comment_root: { $exists: false },
-                                reply_user: { $exists: false }
-                            }
-                        },
-                        {
-                            $lookup: {
-                                "from": "users",
-                                "let": { user_id: "$user" },
-                                "pipeline": [
-                                    { $match: { $expr: { $eq: ["$_id", "$$user_id"] } } },
-                                    { $project: { name: 1, avatar: 1 } }
-                                ],
-                                "as": "user"
-                            }
-                        },
-                        { $unwind: "$user" },
-                        {
-                            $lookup: {
-                                "from": "comments",
-                                "let": { cm_id: "$replyCM" },
-                                "pipeline": [
-                                    { $match: { $expr: { $in: ["$_id", "$$cm_id"] } } },
-                                    {
-                                        $lookup: {
-                                            "from": "users",
-                                            "let": { user_id: "$user" },
-                                            "pipeline": [
-                                                { $match: { $expr: { $eq: ["$_id", "$$user_id"] } } },
-                                                { $project: { name: 1, avatar: 1 } }
-                                            ],
-                                            "as": "user"
-                                        }
-                                    },
-                                    { $unwind: "$user" },
-                                    {
-                                        $lookup: {
-                                            "from": "users",
-                                            "let": { user_id: "$reply_user" },
-                                            "pipeline": [
-                                                { $match: { $expr: { $eq: ["$_id", "$$user_id"] } } },
-                                                { $project: { name: 1, avatar: 1 } }
-                                            ],
-                                            "as": "reply_user"
-                                        }
-                                    },
-                                    { $unwind: "$reply_user" }
-                                ],
-                                "as": "replyCM"
-                            }
-                        },
-                        { $sort: { createdAt: -1 } },
-                        { $skip: skip },
-                        { $limit: limit }
-                    ],
-                    totalCount: [
-                        {
-                            $match: {
-                                blog_id: new mongoose.Types.ObjectId(req.params.id),
-                                comment_root: { $exists: false },
-                                reply_user: { $exists: false }
-                            }
-                        },
-                        { $count: 'count' }
-                    ]
-                }
-            },
-            {
-                $project: {
-                    count: { $arrayElemAt: ["$totalCount.count", 0] },
-                    totalData: 1
-                }
-            }
-        ])
-        const comments = data[0].totalData;
-        const count = data[0].count;
-
-        let total = 0;
-
-        if (count % limit === 0) {
-            total = count / limit;
-        } else {
-            total = Math.floor(count / limit) + 1;
-        }
-
-        return res.json({ comments, total })
-
-    } catch (error) {
-        next(error)
-    }
-    }
-    
-    replyComment = async (req: IReqAuth, res: Response,next:NextFunction) => {
-       try {
+    createComment = async (req: IReqAuth, res: Response, next: NextFunction) => {
+        try {
             if (!req.user) throw ApiError.UnauthorizedError();
-        const {
-            content,
-            blog_id,
-            blog_user_id,
-            comment_root,
-            reply_user
-        } = req.body
+            const {
+                content,
+                blog_id,
+                blog_user_id
+            } = req.body
 
+            const newComment = new Comments({
+                user: req.user._id,
+                content,
+                blog_id,
+                blog_user_id
+            });
+            await newComment.save()
 
-        const newComment = new Comments({
-            user: req.user._id,
-            content,
-            blog_id,
-            blog_user_id,
-            comment_root,
-            reply_user: reply_user._id
-        })
-
-        await Comments.findOneAndUpdate({ _id: comment_root }, {
-            $push: { replyCM: newComment._id }
-        })
-
-        await newComment.save()
-
-        return res.json(newComment)
-
-    } catch (error) {
-        next(error)
+            return res.json(newComment)
+        } catch (error) {
+            next(error)
+        }
     }
+
+    getComments = async (req: Request, res: Response, next: NextFunction) => {
+        const { limit, skip } = Pagination(req)
+
+        try {
+            const data = await Comments.aggregate([
+                {
+                    $facet: {
+                        totalData: [
+                            {
+                                $match: {
+                                    blog_id: new mongoose.Types.ObjectId(req.params.id),
+                                    comment_root: { $exists: false },
+                                    reply_user: { $exists: false }
+                                }
+                            },
+                            {
+                                $lookup: {
+                                    "from": "users",
+                                    "let": { user_id: "$user" },
+                                    "pipeline": [
+                                        { $match: { $expr: { $eq: ["$_id", "$$user_id"] } } },
+                                        { $project: { name: 1, avatar: 1 } }
+                                    ],
+                                    "as": "user"
+                                }
+                            },
+                            { $unwind: "$user" },
+                            {
+                                $lookup: {
+                                    "from": "comments",
+                                    "let": { cm_id: "$replyCM" },
+                                    "pipeline": [
+                                        { $match: { $expr: { $in: ["$_id", "$$cm_id"] } } },
+                                        {
+                                            $lookup: {
+                                                "from": "users",
+                                                "let": { user_id: "$user" },
+                                                "pipeline": [
+                                                    { $match: { $expr: { $eq: ["$_id", "$$user_id"] } } },
+                                                    { $project: { name: 1, avatar: 1 } }
+                                                ],
+                                                "as": "user"
+                                            }
+                                        },
+                                        { $unwind: "$user" },
+                                        {
+                                            $lookup: {
+                                                "from": "users",
+                                                "let": { user_id: "$reply_user" },
+                                                "pipeline": [
+                                                    { $match: { $expr: { $eq: ["$_id", "$$user_id"] } } },
+                                                    { $project: { name: 1, avatar: 1 } }
+                                                ],
+                                                "as": "reply_user"
+                                            }
+                                        },
+                                        { $unwind: "$reply_user" }
+                                    ],
+                                    "as": "replyCM"
+                                }
+                            },
+                            { $sort: { createdAt: -1 } },
+                            { $skip: skip },
+                            { $limit: limit }
+                        ],
+                        totalCount: [
+                            {
+                                $match: {
+                                    blog_id: new mongoose.Types.ObjectId(req.params.id),
+                                    comment_root: { $exists: false },
+                                    reply_user: { $exists: false }
+                                }
+                            },
+                            { $count: 'count' }
+                        ]
+                    }
+                },
+                {
+                    $project: {
+                        count: { $arrayElemAt: ["$totalCount.count", 0] },
+                        totalData: 1
+                    }
+                }
+            ])
+            const comments = data[0].totalData;
+            const count = data[0].count;
+
+            let total = 0;
+
+            if (count % limit === 0) {
+                total = count / limit;
+            } else {
+                total = Math.floor(count / limit) + 1;
+            }
+
+            return res.json({ comments, total })
+
+        } catch (error) {
+            next(error)
+        }
     }
-    
-    updateComment = async (req: IReqAuth, res: Response,next:NextFunction) => {
+
+    replyComment = async (req: IReqAuth, res: Response, next: NextFunction) => {
+        try {
+            if (!req.user) throw ApiError.UnauthorizedError();
+            const {
+                content,
+                blog_id,
+                blog_user_id,
+                comment_root,
+                reply_user
+            } = req.body
+
+
+            const newComment = new Comments({
+                user: req.user._id,
+                content,
+                blog_id,
+                blog_user_id,
+                comment_root,
+                reply_user: reply_user._id
+            })
+
+            await Comments.findOneAndUpdate({ _id: comment_root }, {
+                $push: { replyCM: newComment._id }
+            })
+
+            await newComment.save()
+
+            return res.json(newComment)
+
+        } catch (error) {
+            next(error)
+        }
+    }
+
+    updateComment = async (req: IReqAuth, res: Response, next: NextFunction) => {
 
 
         try {
             if (!req.user) throw ApiError.UnauthorizedError();
-        const { content } = req.body
+            const { content } = req.body
 
-        const comment = await Comments.findOneAndUpdate({
-            _id: req.params.id, user: req.user.id
-        }, { content })
+            const comment = await Comments.findOneAndUpdate({
+                _id: req.params.id, user: req.user.id
+            }, { content })
 
-        if (!comment)
-            return res.status(400).json({ msg: "Comment does not exits." })
+            if (!comment) {
+                throw ApiError.BadRequest("Comment does not exits.")
+            }
 
-        return res.json({ msg: "Update Success!" })
 
-    } catch (error) {
-next(error)    }
-    }
-    
-deleteComment = async (req: IReqAuth, res: Response,next:NextFunction) => {
+            return res.json({ message: "Update Success!" })
 
-    try {
-        if (!req.user) throw ApiError.UnauthorizedError();
-        const comment = await Comments.findOneAndDelete({
-            _id: req.params.id,
-            $or: [
-                { user: req.user._id },
-                { blog_user_id: req.user._id }
-            ]
-        })
-
-        if (!comment)
-            return res.status(400).json({ msg: "Comment does not exits." })
-
-        if (comment.comment_root) {
-            // update replyCM
-            await Comments.findOneAndUpdate({ _id: comment.comment_root }, {
-                $pull: { replyCM: comment._id }
-            })
-        } else {
-            // delete all comments in replyCM
-            await Comments.deleteMany({ _id: { $in: comment.replyCM } })
+        } catch (error) {
+            next(error)
         }
-
-        return res.json({ msg: "Delete Success!" })
-
-    } catch (error) {
-        next(error)
     }
-}
+
+    deleteComment = async (req: IReqAuth, res: Response, next: NextFunction) => {
+
+        try {
+            if (!req.user) throw ApiError.UnauthorizedError();
+            const comment = await Comments.findOneAndDelete({
+                _id: req.params.id,
+                $or: [
+                    { user: req.user._id },
+                    { blog_user_id: req.user._id }
+                ]
+            })
+
+            if (!comment) throw ApiError.BadRequest("Comment does not exits.")
+
+            if (comment.comment_root) {
+                // update replyCM
+                await Comments.findOneAndUpdate({ _id: comment.comment_root }, {
+                    $pull: { replyCM: comment._id }
+                })
+            } else {
+                // delete all comments in replyCM
+                await Comments.deleteMany({ _id: { $in: comment.replyCM } })
+            }
+
+            return res.json({ message: "Delete Success!" })
+
+        } catch (error) {
+            next(error)
+        }
+    }
 }
 
 export default new CommentController()
